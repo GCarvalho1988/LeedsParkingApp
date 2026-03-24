4710e82a729a8073595355ef8e6d4bddce1f1d1d
Guilherme Carvalho <Guilherme.Carvalho@sectra.com>
2026-03-24 10:43:38 +0000

    feat: rewrite get-employees to use Netlify Blobs

--- netlify/functions/get-employees.js
+++ netlify/functions/get-employees.js
@@ -1,9 +1,9 @@
-exports.handler = async () => {
-  const res = await fetch(process.env.FLOW_GET_EMPLOYEES, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: '{}',
-  });
-  const data = await res.json();
-  return { statusCode: res.status, body: JSON.stringify(data) };
+import { readBlob } from './_blob-helpers.js';
+
+export const handler = async () => {
+  const employees = await readBlob('employees');
+  if (employees === null) {
+    return { statusCode: 500, body: JSON.stringify({ error: 'storageError' }) };
+  }
+  return { statusCode: 200, body: JSON.stringify(employees) };
 };
